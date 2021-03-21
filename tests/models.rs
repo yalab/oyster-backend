99da389973f24caff688f7e7b8808dcef9ecc04b
yalab <rudeboyjet@gmail.com>
2021-03-21 12:42:01 +0900

    first http get impl.

--- tests/models.rs
+++ tests/models.rs
@@ -1,6 +1,8 @@
 use diesel::prelude::*;
 use oyster::models::*;
 use oyster::schema::sites::dsl::*;
+use std::collections::HashMap;
+use tokio;
 
 #[test]
 fn download_test() {
@@ -10,5 +12,11 @@ fn download_test() {
         Ok(site) => site,
         Err(_)   => panic!("No such site"),
     };
+
+    let get =  async {
+      let resp = reqwest::get("https://httpbin.org/ip");
+      println!("{:#?}", resp.await);
+    };
+    tokio::runtime::Runtime::new().unwrap().block_on(get);
     println!("Site {}", site.url);
 }

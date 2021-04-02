bd3438747c72ab84bc6d024cfc767a7b1b1cf145
yalab <rudeboyjet@gmail.com>
2021-04-02 09:12:54 +0900

    Download first http access;

--- src/models.rs
+++ src/models.rs
@@ -1,4 +1,5 @@
 use super::schema::sites;
+use tokio;
 
 #[derive(Queryable)]
 
@@ -6,6 +7,21 @@ pub struct Site {
     pub id: i32,
     pub url: String,
     pub crawled_at: Option<std::time::SystemTime>,
+}
+
+impl Site {
+    pub fn get(&self) -> reqwest::Response {
+        let url = &self.url;
+        let m =  async {
+            let result = reqwest::get(url).await;
+            let response = match result {
+                Ok(response) => response,
+                Err(_) => panic!("Response Error"),
+            };
+            return response
+        };
+        return tokio::runtime::Runtime::new().unwrap().block_on(m)
+    }
 }
 
 #[derive(Insertable)]
--- tests/models.rs
+++ tests/models.rs
@@ -1,8 +1,6 @@
 use diesel::prelude::*;
 use oyster::models::*;
 use oyster::schema::sites::dsl::*;
-use std::collections::HashMap;
-use tokio;
 
 #[test]
 fn download_test() {
@@ -12,11 +10,6 @@ fn download_test() {
         Ok(site) => site,
         Err(_)   => panic!("No such site"),
     };
-
-    let get =  async {
-      let resp = reqwest::get("https://httpbin.org/ip");
-      println!("{:#?}", resp.await);
-    };
-    tokio::runtime::Runtime::new().unwrap().block_on(get);
-    println!("Site {}", site.url);
+    let response = site.get();
+    assert_eq!(200, response.status());
 }

2c0b4e1ef10052ca524b8d0191dc5dfc7ec36453
yalab <rudeboyjet@gmail.com>
2021-04-04 17:12:19 +0900

    T

--- tests/models.rs
+++ tests/models.rs
@@ -2,6 +2,10 @@ use diesel::prelude::*;
 use oyster::models::*;
 use oyster::schema::sites::dsl::*;
 
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+
 #[test]
 fn download_test() {
     let connection = oyster::establish_connection();
@@ -15,6 +19,21 @@ fn download_test() {
     let m = async {
         return response.text().await
     };
-    let body = tokio::runtime::Runtime::new().unwrap().block_on(m);
+    let body = match tokio::runtime::Runtime::new().unwrap().block_on(m){
+        Ok(body) => body,
+        Err(_) => panic!("Error body")
+    };
     println!("{:#?}", body);
+
+    let path = Path::new("test.html");
+    let display = path.display();
+    let mut file = match File::create(&path) {
+        Err(why) => panic!("couldn't create {}: {}", display, why),
+        Ok(file) => file,
+    };
+
+    match file.write_all(body.as_bytes()) {
+        Err(why) => panic!("couldn't write to {}: {}", display, why),
+        Ok(_) => println!("successfully wrote to {}", display),
+    }
 }

dec1f5f7ba0c107e37300099e71bee76cca34253
yalab <rudeboyjet@gmail.com>
2021-04-29 15:22:55 +0900

    Reafactoring Site->fetch

--- src/models.rs
+++ src/models.rs
@@ -8,6 +8,15 @@ pub struct Site {
     pub crawled_at: Option<std::time::SystemTime>,
 }
 
+impl Site {
+    pub fn fetch(&self) -> String {
+       let m = async {
+            return reqwest::get(&self.url).await.unwrap().text().await;
+        };
+        return tokio::runtime::Runtime::new().unwrap().block_on(m).unwrap();
+    }
+}
+
 #[derive(Insertable)]
 #[table_name = "sites"]
 pub struct NewSite<'a> {
--- tests/models.rs
+++ tests/models.rs
@@ -2,27 +2,12 @@ use diesel::prelude::*;
 use oyster::models::*;
 use oyster::schema::sites::dsl::*;
 
-use std::fs::File;
-use std::io::prelude::*;
-use std::path::Path;
-
 #[test]
 fn download_test() {
     let connection = oyster::establish_connection();
-    let r = sites.find(1).get_result::<Site>(&connection).unwrap();
-    let m = async {
-        return reqwest::get(r.url).await.unwrap().text().await;
-    };
-    let body = tokio::runtime::Runtime::new().unwrap().block_on(m);
-    let path = Path::new("test.html");
-    let display = path.display();
-    let mut file = match File::create(&path) {
-        Err(why) => panic!("couldn't create {}: {}", display, why),
-        Ok(file) => file,
-    };
-
-    match file.write_all(body.unwrap().as_bytes()) {
-        Err(why) => panic!("couldn't write to {}: {}", display, why),
-        Ok(_) => println!("successfully wrote to {}", display),
-    }
+    let site = sites.find(1).get_result::<Site>(&connection).unwrap();
+    let body = site.fetch();
+    assert_eq!(&body[0..15], "<!DOCTYPE html>");
+    let len = body.len();
+    assert_eq!(&body[(len - 9)..(len - 2)], "</html>");
 }
